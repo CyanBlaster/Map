14586edf97cb8b0bf13763609789d55e3f36fd44
CyanBlaster <michael.ng.do@gmail.com>
2025-06-27 22:06:18 -0400

    Fixed the selector, and made the balls swappable.

--- MapTest.py
+++ MapTest.py
@@ -11,8 +11,10 @@ pygame.init()
 def ZeroField(n):
     return [[0] * n for i in range(n)]
 
-def randomColor():
-    x = random.randint(1, 5)
+def randomInteger():
+    return random.randint(1, 5)
+    
+def Color(x):
     if(x == 1):
          return (255, 0, 0)
     elif(x == 2):
@@ -28,15 +30,25 @@ def main():
     xIdx = 0
     yIdx = 0
     cellAmount = 40
+    savedX = 0
+    savedY = 0
+    selected = False
     screen = pygame.display.set_mode((width, height))
     running = True
     map = ZeroField(cellAmount)
     for y in range (cellAmount):
         for x in range(cellAmount):
-            pygame.draw.circle(screen, randomColor(), (x * cellAmount + 19, y * cellAmount + 19), 19)
+            a = randomInteger()
+            map[x][y] = a
+            pygame.draw.circle(screen, Color(map[x][y]), (x * cellAmount + 19, y * cellAmount + 19), 19)
 
     while running:
-            
+            for y in range (cellAmount):
+                for x in range(cellAmount):
+                  pygame.draw.circle(screen, Color(map[x][y]), (x * cellAmount + 19, y * cellAmount + 19), 19)
+
+
+
             pygame.draw.rect(screen, (255, 255, 255), (xIdx * cellAmount - 1, yIdx * cellAmount - 1, 40, 40), 1, border_radius = 1)
             pygame.display.flip()
             for events in pygame.event.get():
@@ -55,5 +67,16 @@ def main():
                     elif events.key == pygame.K_DOWN:
                         pygame.draw.rect(screen, (0, 0, 0), (xIdx * cellAmount - 1, yIdx * cellAmount - 1, 40, 40), 1, border_radius = 1)
                         yIdx += 1
+                    elif events.key == pygame.K_SPACE:
+                        if(selected == False):
+                            selected = True
+                            savedX = xIdx
+                            savedY = yIdx
+                        else:
+                            if(abs(savedX - xIdx) <= 1 and abs(savedY - yIdx) <= 1):
+                                selected = False
+                                a = map[savedX][savedY]
+                                map[savedX][savedY] = map[xIdx][yIdx]
+                                map[xIdx][yIdx] = a
                         
 main()
