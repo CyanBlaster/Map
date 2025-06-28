1a885ce47f10ada3ce425b556511da52db695188
CyanBlaster <michael.ng.do@gmail.com>
2025-06-27 21:37:19 -0400

    Made a working map and movable non-working selector.

---
+++ MapTest.py
@@ -0,0 +1,59 @@
+import sys
+import pygame
+import datetime
+import math
+import pygame.freetype
+import random
+
+pygame.init()
+
+
+def ZeroField(n):
+    return [[0] * n for i in range(n)]
+
+def randomColor():
+    x = random.randint(1, 5)
+    if(x == 1):
+         return (255, 0, 0)
+    elif(x == 2):
+       return (0, 255, 0)
+    elif(x == 3):
+       return (0, 0, 255)
+    elif(x == 4):
+       return (255, 0, 255)
+    return (0, 0, 0)
+def main():
+    width = 800
+    height = 800
+    xIdx = 0
+    yIdx = 0
+    cellAmount = 40
+    screen = pygame.display.set_mode((width, height))
+    running = True
+    map = ZeroField(cellAmount)
+    for y in range (cellAmount):
+        for x in range(cellAmount):
+            pygame.draw.circle(screen, randomColor(), (x * cellAmount + 19, y * cellAmount + 19), 19)
+
+    while running:
+            
+            pygame.draw.rect(screen, (255, 255, 255), (xIdx * cellAmount - 1, yIdx * cellAmount - 1, 40, 40), 1, border_radius = 1)
+            pygame.display.flip()
+            for events in pygame.event.get():
+                if events.type == pygame.QUIT:
+                    running = False
+                if events.type == pygame.KEYDOWN:
+                    if events.key == pygame.K_LEFT:
+                        pygame.draw.rect(screen, (0, 0, 0), (xIdx * cellAmount - 1, yIdx * cellAmount - 1, 40, 40), 1, border_radius = 1)
+                        xIdx -= 1
+                    elif events.key == pygame.K_RIGHT:
+                        pygame.draw.rect(screen, (0, 0, 0), (xIdx * cellAmount - 1, yIdx * cellAmount - 1, 40, 40), 1, border_radius = 1)
+                        xIdx += 1
+                    elif events.key == pygame.K_UP:
+                        pygame.draw.rect(screen, (0, 0, 0), (xIdx * cellAmount - 1, yIdx * cellAmount - 1, 40, 40), 1, border_radius = 1)
+                        yIdx -= 1
+                    elif events.key == pygame.K_DOWN:
+                        pygame.draw.rect(screen, (0, 0, 0), (xIdx * cellAmount - 1, yIdx * cellAmount - 1, 40, 40), 1, border_radius = 1)
+                        yIdx += 1
+                        
+main()
